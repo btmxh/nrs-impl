8d41d7a1cc8507365935d0c8a0512d01b67dbc10
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-08-25 00:10:01 +0700

    [mod] [...] Motokano datta e8

--- impl/src/main/kotlin/com/dah/nrs/seasonal/Summer2022.kt
+++ impl/src/main/kotlin/com/dah/nrs/seasonal/Summer2022.kt
@@ -104,7 +104,7 @@ fun DSLScope.Summer2022() {
 
         // still shit anime anyways
         // lmfao they used "inverse-killedby" method to not be dropped
-        Progress(Boredom.Watching, 7)
+        Progress(Boredom.Watching, 8)
         KilledBy("F-VGMDB-4499", potential = 0.1, effect = 0.1)
         KilledBy("M-VGMDB-AL-76155-2", potential = 0.1, effect = 0.75)
     }
