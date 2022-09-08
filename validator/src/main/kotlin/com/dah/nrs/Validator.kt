140a94cb5ec2ec0c4a138ad496fd24fe6487529c
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-09-08 18:47:47 +0700

    feat(validator): :white_check_mark: Added dah_no_title rule

--- validator/src/main/kotlin/com/dah/nrs/Validator.kt
+++ validator/src/main/kotlin/com/dah/nrs/Validator.kt
@@ -58,7 +58,8 @@ fun main() {
         dah_null_entry_reference(data),
         dah_anime_no_boredom(data),
         dah_uncompleted_no_progress(data),
-        dah_invalid_id(data)
+        dah_invalid_id(data),
+        dah_no_title(data),
     )) {
         rule.run()
     }
