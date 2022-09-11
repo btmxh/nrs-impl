d301b11854ff84190eeb57f184df002b0452593b
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-09-11 13:23:06 +0700

    chore(impl): :heavy_plus_sign: Extreme Hearts E10

--- impl/src/main/kotlin/com/dah/nrs/seasonal/Summer2022.kt
+++ impl/src/main/kotlin/com/dah/nrs/seasonal/Summer2022.kt
@@ -159,7 +159,7 @@ fun DSLScope.Summer2022() {
 
         Visual(VisualKind.Animated, 0.5, 0.1)
 
-        Progress(Boredom.Watching, 9)
+        Progress(Boredom.Watching, 10)
         KilledBy("F-VGMDB-4499", potential = 0.5, effect = 0.1)
         KilledBy("M-VGMDB-AL-76155-2", potential = 0.5, effect = 0.75)
     }
