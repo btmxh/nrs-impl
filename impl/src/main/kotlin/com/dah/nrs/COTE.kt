7cb29371f71bdb687b4ec5bff57de491314597df
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-09-14 00:28:58 +0700

    chore(impl): :heavy_plus_sign: Youkoso Jitsuryoku [...] S2E11

--- impl/src/main/kotlin/com/dah/nrs/COTE.kt
+++ impl/src/main/kotlin/com/dah/nrs/COTE.kt
@@ -51,7 +51,7 @@ fun DSLScope.COTE() {
             // we are not the same
             // (trust me u don't want us to be the same lmfao)
 
-            AnimeProgressOld(Boredom.Watching, 10)
+            AnimeProgressOld(Boredom.Watching, 11)
             // a lil bit worse than s1 (but still not that bad)
             Visual(VisualKind.Animated, 0.6, 0.2)
 
