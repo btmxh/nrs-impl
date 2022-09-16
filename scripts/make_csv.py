61a4576cac3ec8637d5a02cfafdef7d4e2f76bc3
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-09-16 14:52:29 +0700

    refactor(scripts): :coffin: Tried to make `make_csv.py` py2-compatible (4)

--- scripts/make_csv.py
+++ scripts/make_csv.py
@@ -28,11 +28,9 @@ def output(id, entry, score): return [
     score['DAH_meta']['DAH_overall_score'],
 ] + score['overallVector']
 
-if sys.version_info >= (3, 0):
-    w = csv.writer(open('output/nrs.csv', 'w', encoding='utf-8', newline=''))
-else:
+w = csv.writer(open('output/nrs.csv', 'wb'))
+if sys.version_info < (3, 0):
     from codecs import open
-    w = csv.writer(open('output/nrs.csv', 'wb', encoding='utf-8'))
 w.writerow(labels)
 entries = json.load(open('output/entries.json', encoding='utf-8'))
 scores = json.load(open('output/scores.json', encoding='utf-8'))
