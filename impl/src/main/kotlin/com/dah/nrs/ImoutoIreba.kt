b19e4cb29155a306d4fd387b40cf44199b305ad7
ngoduyanh <ngoduyanh@users.noreply.github.com>
2022-10-28 11:06:09 +0000

    chore(impl): :bookmark: Filled metadata

--- impl/src/main/kotlin/com/dah/nrs/ImoutoIreba.kt
+++ impl/src/main/kotlin/com/dah/nrs/ImoutoIreba.kt
@@ -10,6 +10,11 @@ fun DSLScope.ImoutoIreba() {
 
         Entry {
             id = "A-MAL-35413"
+            title = "Imouto sae Ireba Ii." // generated(fill_anime_metadata.dart v0.1.1)
+            idAniDB = 35413 // generated(fill_anime_metadata.dart v0.1.1)
+            idKitsu = 11207 // generated(fill_anime_metadata.dart v0.1.1)
+            idAniList = 98596 // generated(fill_anime_metadata.dart v0.1.1)
+            idMAL = 35413 // generated(fill_anime_metadata.dart v0.1.1)
             bestGirl = "Miyako Shirakawa" // generated(generate_best_girls.dart v0.1.0)
 
             AnimeProgressOld(Boredom.Completed, 12)
@@ -21,9 +26,12 @@ fun DSLScope.ImoutoIreba() {
 
         Entry {
             id = "M-VGMDB-AL-70621"
+            title = "Ashita no Kimi sae Ireba Ii. / ChouCho" // generated(fill_music_metadata.dart v0.1.1)
             Visual(VisualKind.AlbumArt, 0.5, 0.2)
 
             SubIDEntry("1") {
+                MusicConsumedProgress("5:04") // generated(fill_music_metadata.dart v0.1.1)
+                title = "Ashita no Kimi sae Ireba Ii." // generated(fill_music_metadata.dart v0.1.1)
                 Music(0.6);
                 OsuSong(personal = 0.5, community = 0.1)
             }
