77af00802cd5212c0621cef9a332d9544a183deb
ngoduyanh <43819821+ngoduyanh@users.noreply.github.com>
2022-11-05 13:05:53 +0700

    Ranked `Dreams come true -Hana Saki Hokoru, Chikyuu no Moto de-` (#88)
    * chore(impl): :sparkles: Ranked `Dreams come true -Hana Saki Hokoru, Chikyuu no Moto de-`
    
    * chore(impl): :bookmark: Filled metadata
    
    Co-authored-by: ngoduyanh <ngoduyanh@users.noreply.github.com>

--- impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
+++ impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
@@ -97,4 +97,16 @@ fun DSLScope.OsuGame() {
             Music(0.7)
         }
     }
+
+    Entry {
+        id = "M-VGMDB-AL-78595"
+        title = "AstralAir no Shiroki Towa - ETERNAL BEST -" // generated(fill_music_metadata.dart v0.1.1)
+
+        Visual(VisualKind.AlbumArt, 0.4, 0.6)
+        SubIDEntry("16") {
+            MusicConsumedProgress("6:10") // generated(fill_music_metadata.dart v0.1.1)
+            title = "Dreams come true -Hana Saki Hokoru, Chikyuu no Moto de-" // generated(fill_music_metadata.dart v0.1.1)
+            Music(0.55)
+        }
+    }
 }
