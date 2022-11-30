625fe23c6e5d0e03edb6f22e158a9cd018fe6d30
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-11-30 12:01:04 +0700

    Revert "chore(core): :art: visual kinds rebalance"
    This reverts commit bf3a79b0f8cfef35d45e2ca3f4fe8b69ad2efef5.

--- core/src/main/kotlin/com/dah/nrs/exts/DAH_standards.kt
+++ core/src/main/kotlin/com/dah/nrs/exts/DAH_standards.kt
@@ -414,13 +414,19 @@ private fun calcVisualScore(b: Double, u: Double): Double {
 
 enum class VisualKind(val baseScore: Double) {
     // A-MAL-38009: https://animixplay.to/v1/restage-dream-days
-    Animated(1.0),
+    Animated(3.0),
 
     // G-VGMDB-1880: https://store.steampowered.com/app/1152310/Atelier_Escha__Logy_Alchemists_of_the_Dusk_Sky_DX/
-    RPG3DGame(1.0),
+    RPG3DGame(2.0),
 
     // A-MAL-34240: https://www.youtube.com/watch?v=fzQ6gRAEoy0
-    AnimatedShort(0.8),
+    AnimatedShort(1.0),
+
+    // (unranked): https://twitter.com/lapi_staff/status/1555750517887975425
+    AnimatedGachaCardArt(1.0),
+
+    // M-VGMDB-AL-100087-1: https://www.youtube.com/watch?v=IqdpYyaLnNc
+    AnimatedMV(1.0),
 
     // M-VGMDB-AL-116297-1: https://www.youtube.com/watch?v=vyaGNvuVDuM
     SemiAnimatedMV(0.8),
@@ -434,17 +440,11 @@ enum class VisualKind(val baseScore: Double) {
     // L-MAL-126146 (unranked): https://en.wikipedia.org/wiki/Oshi_no_Ko#/media/File:Oshi_no_Ko_Volume_1.jpg
     Manga(0.8),
 
-    // (unranked): https://twitter.com/lapi_staff/status/1555750517887975425
-    AnimatedGachaCardArt(0.7),
-
-    // M-VGMDB-AL-100087-1: https://www.youtube.com/watch?v=IqdpYyaLnNc
-    AnimatedMV(0.5),
-
     // L-MAL-89357 (unranked): https://danbooru.donmai.us/posts/2905913
-    LightNovel(0.6),
+    LightNovel(0.5),
 
     // M-VGMDB-AL-121168 (not canon): https://www.youtube.com/watch?v=hj_4YAVmmuI
-    StaticMV(0.4),
+    StaticMV(0.25),
 
     // M-VGMDB-AL-121168 (canon): https://medium-media.vgm.io/albums/86/121168/121168-de8dcf1b4ceb.jpg
     AlbumArt(0.25),
@@ -458,7 +458,7 @@ fun AcceptImpact.Visual(kind: VisualKind, base: Double, uniqueness: Double, bloc
     Impact {
         description = "Generic visual"
         score = vector {
-            set(Art.Visual, calcVisualScore(base, uniqueness) * kind.baseScore * 2)
+            set(Art.Visual, calcVisualScore(base, uniqueness) * kind.baseScore)
         }
         meta("type", "DAH_nonstandard_generic_visual")
         meta("base", base)
