a4c5c95590d1e086617f70af3e6811fd0948f86b
ngoduyanh <43819821+ngoduyanh@users.noreply.github.com>
2022-12-24 22:22:19 +0700

    chore(impl): :sparkles: rank `Euphorium` (#328)
    * chore(impl): :sparkles: rank `Euphorium`
    
    * chore(impl): :bookmark: Filled metadata
    
    Co-authored-by: ngoduyanh <ngoduyanh@users.noreply.github.com>

--- impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
+++ impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
@@ -103,6 +103,15 @@ fun DSLScope.OsuGame() {
         title = "AstralAir no Shiroki Towa - ETERNAL BEST -" // generated(fill_music_metadata.dart v0.1.1)
 
         Visual(VisualKind.AlbumArt, 0.4, 0.6)
+
+        SubIDEntry("9") {
+            MusicConsumedProgress("4:44") // generated(fill_music_metadata.dart v0.1.1)
+            title = "Euphorium" // generated(fill_music_metadata.dart v0.1.1)
+            Music(0.45)
+            OsuSong(personal = 0.3, community = 0.3)
+            // mrekk pp haha
+        }
+
         SubIDEntry("16") {
             MusicConsumedProgress("6:10") // generated(fill_music_metadata.dart v0.1.1)
             title = "Dreams come true -Hana Saki Hokoru, Chikyuu no Moto de-" // generated(fill_music_metadata.dart v0.1.1)
