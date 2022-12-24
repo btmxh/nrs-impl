fb70089dda5a6c0709e42ce5217a4715348733df
ngoduyanh <43819821+ngoduyanh@users.noreply.github.com>
2022-12-24 23:47:07 +0700

    chore(impl): :sparkles: rank `Have a live day` & `Ringing＊Days` (#339)
    * chore(impl): :sparkles: rank `Have a live day` & `Ringing＊Days`
    
    * fix(impl): :ambulance: specify `Ringing＊Days` length
    
    * chore(impl): :bookmark: Filled metadata
    
    Co-authored-by: ngoduyanh <ngoduyanh@users.noreply.github.com>

--- impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
+++ impl/src/main/kotlin/com/dah/nrs/OsuGame.kt
@@ -134,6 +134,32 @@ fun DSLScope.OsuGame() {
             Music(0.65)
         }
     }
+
+    Entry {
+        id = "M-VGMDB-AL-79717"
+        title = "Shirokoi Sakura*Gram THEME SONG COLLECTION! 4" // generated(fill_music_metadata.dart v0.1.1)
+
+        Visual(VisualKind.AlbumArt, 0.5, 0.3)
+        SubIDEntry("1") {
+            MusicConsumedProgress("4:29") // generated(fill_music_metadata.dart v0.1.1)
+            title = "Have a live day fullver." // generated(fill_music_metadata.dart v0.1.1)
+            Music(0.55)
+        }
+    }
+
+    Entry {
+        id = "M-VGMDB-AL-117099"
+        title = "HANAGANE KANADE*GRAM THEME SONG COLLECTION! 5" // generated(fill_music_metadata.dart v0.1.1)
+
+        Visual(VisualKind.AlbumArt, 0.5, 0.3)
+        SubIDEntry("1") {
+            title = "Ringing＊Days fullver." // generated(fill_music_metadata.dart v0.1.1)
+            // Length source: https://www.youtube.com/watch?v=d6KEr9wLTm4
+            MusicConsumedProgress("5:07") // impl_overridden
+            Music(0.6)
+        }
+    }
+
     Entry {
         id = "M-VGMDB-AL-99204"
         title = "SMASH The PAINT!! / Nijisanji" // generated(fill_music_metadata.dart v0.1.1)
