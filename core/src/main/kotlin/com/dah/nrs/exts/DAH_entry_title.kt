406baa14698472f65764a188b64235d09d5dcb90
ngoduyanh <ngoduyanh.chip@gmail.com>
2023-04-06 21:40:43 +0700

    refactor: :fire: absolute destruction

--- core/src/main/kotlin/com/dah/nrs/exts/DAH_entry_title.kt
+++
@@ -1,6 +0,0 @@
-package com.dah.nrs.exts
-
-import com.dah.nrs.dsl.DSLEntry
-import com.dah.nrs.dsl.stringMeta
-
-var DSLEntry.title by stringMeta("DAH_entry_title")
--- core/src/main/kotlin/com/dah/nrs/exts/DAH_meta.kt
+++
@@ -1,6 +0,0 @@
-package com.dah.nrs.exts
-
-import com.dah.nrs.core.Extension
-import com.dah.nrs.core.NRSContextBuilder
-
-class DAH_meta(builder: NRSContextBuilder) : Extension(builder)
