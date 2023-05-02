fe657388bda597fb910a2f4fc62165f77dced098
ngoduyanh <ngoduyanh.chip@gmail.com>
2023-05-02 15:13:19 +0700

    build(scripts): :arrow_up: use `deps.ts` and upgrade deps

---
+++ scripts/deps.ts
@@ -0,0 +1,8 @@
+export {
+  newContext,
+  processContext,
+} from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v0.1.2/mod.ts";
+export {
+  FileResolver,
+  processNRSXML,
+} from "https://raw.githubusercontent.com/ngoduyanh/nrsml/v0.1.4/mod.ts";
